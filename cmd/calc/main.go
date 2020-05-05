16e892522c86a79457d1dd968f5d670ac00b7b5e
drittweiler <ripx80@protonmail.com>
2020-05-05 14:01:31 +0200

    add scale cmd

--- cmd/calc/main.go
+++ cmd/calc/main.go
@@ -2,48 +2,20 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/ripx80/recipe"
-	"github.com/ripx80/recipe/pkgs/plato"
 )
 
-const in string = "../../testdata/ipa.json"
-
-/*
-	Skalieren bei Änderung
-	[ ] Schüttung
-	[ ] Ausschlagwürze
-	[ ] Sudausbeute
-
-	Schüttung: SumMalt
-
-	Ausschlagwürze: DecisiveSeasoning AM
-	Sudausbeute: 	SudYield
-
-	add tablewriter for output plato table
-*/
-
 func main() {
-	recipe, err := recipe.LoadFile(in, &recipe.M3{})
+	rec, err := recipe.LoadFile("../../testdata/ipa.json", &recipe.M3{})
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(2)
+		return
 	}
-
-	Plato := plato.New()
-
-	//fmt.Println(recipe)
-	_, err = Plato.SW(recipe.Global.OriginalWort)
-	//fmt.Println(e)
-
-	newam := 25
-	fmt.Printf("original AM:\t %f\n", recipe.Global.DecisiveSeasoning)
-	fmt.Printf("new AM:\t\t %d\n", newam)
-
-	// e, err = Plato.SG(1.06846)
-	// fmt.Println(e)
-	// e, err = Plato.AF(17.35)
-	// fmt.Println(e)
-
+	rScale, err := rec.Scale(30.0, 70.0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(rScale)
 }
